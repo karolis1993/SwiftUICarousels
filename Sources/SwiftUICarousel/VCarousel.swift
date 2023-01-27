e4307586136884d9d76c0dfea5530cbe1b16f79f
Karolis <karolis199319@gmail.com>
2023-01-27 15:07:48 +0200

    Hide VCarousel until implementation is done

--- Sources/SwiftUICarousel/VCarousel.swift
+++ Sources/SwiftUICarousel/VCarousel.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-public struct VCarousel<Item: View>: View {
+struct VCarousel<Item: View>: View {
     private let sideVisibility: CGFloat
     private let spacing: CGFloat
     private let itemCount: Int
@@ -14,7 +14,7 @@ public struct VCarousel<Item: View>: View {
     @State private var transforms: [CGFloat]
     @State private var didUpdatePage = false
 
-    public init(
+    init(
         numberOfItems: Int,
         sideVisibility: CGFloat = 64,
         itemSpacing: CGFloat = 8,
@@ -29,7 +29,7 @@ public struct VCarousel<Item: View>: View {
         self.transforms = [CGFloat](repeating: 0.8, count: numberOfItems)
     }
 
-    public var body: some View {
+    var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
